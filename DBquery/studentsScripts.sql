bf5bfcb7a89c588db07afb7f34fea75035c8af60
racheev <racheev.sa.dev@gmail.com>
2021-10-21 03:29:04 -0500

    0. Что такое база данных. [#6860]

---
+++ DBquery/studentsScripts.sql
@@ -0,0 +1,15 @@
+create table students(
+	id serial primary key,
+	name varchar(255),
+	age int,
+	gender char,
+	isForeigner bool
+);
+
+insert into students (name, age, gender, isForeigner) values ('Semyon', 34, 'm', false);
+
+select * from students;
+
+update students set isForeigner = true;
+
+delete from students;
