497a0dc60197191d5a65f4dfcbb7d9e1a1704189
racheev <racheev.sa.dev@gmail.com>
2021-10-21 17:12:59 -0500

    0. Что такое обобщенные типы (generics) [#4952 #218398] 5.1.2. Создать итератор четные числа [#150 #218355] Пытаюсь избавиться от ошибки бота. Исправления по замечаниям

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -7,16 +7,26 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     private int[] data;
     private int index;
+    private int nextIndex;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
         this.index = -1;
+        this.nextIndex = -1;
+    }
+
+    private boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean hasNext() {
         for (int i = index + 1; i < data.length; i++) {
-            if (data[i] % 2 == 0) {
+            if (isEven(data[i])) {
+                nextIndex = i;
                 return true;
             }
         }
@@ -28,12 +38,7 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        for (int i = index + 1; i < data.length; i++) {
-            if (data[i] % 2 == 0) {
-                index = i;
-                return data[index];
-            }
-        }
-        throw new NoSuchElementException();
+        index = nextIndex;
+        return data[index];
     }
 }
