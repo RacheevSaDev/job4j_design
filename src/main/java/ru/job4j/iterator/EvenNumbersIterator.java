dce7b40b774c0eac1c72e922f2ffb5e1a33ba874
racheev <racheev.sa.dev@gmail.com>
2021-11-13 01:06:37 -0500

    5.1.2. Создать итератор четные числа [#150 #218355] Исправление. Вся логика в hasNext()

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -7,28 +7,18 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     private int[] data;
     private int index;
-    private int nextIndex;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
-        this.index = -1;
-        this.nextIndex = -1;
-    }
-
-    private boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
-        }
-        return false;
     }
 
     @Override
     public boolean hasNext() {
-        for (int i = index + 1; i < data.length; i++) {
-            if (isEven(data[i])) {
-                nextIndex = i;
+        while (index < data.length) {
+            if (data[index] % 2 == 0) {
                 return true;
             }
+            index++;
         }
         return false;
     }
@@ -38,7 +28,6 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        index = nextIndex;
-        return data[index];
+        return data[index++];
     }
 }
