49411b9df207c60eda6c3319c9b70f64b79c5a46
racheev <racheev.sa.dev@gmail.com>
2021-11-14 16:12:26 -0500

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,78 @@
+package ru.job4j.list;
+
+import java.util.*;
+
+public class SimpleArrayList<T> implements List<T> {
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length) {
+            this.container  = Arrays.copyOf(container, container.length * 2);
+        }
+        this.container[size] = value;
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, container.length);
+        T prevElement = container[index];
+        container[index] = newValue;
+        modCount++;
+        return prevElement;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, container.length);
+        T prevElement = container[index];
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[size - 1] = null;
+        size--;
+        modCount++;
+        return prevElement;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, container.length);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index;
+            final private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return index != size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return container[index++];
+            }
+        };
+    }
+}
