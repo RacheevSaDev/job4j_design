44c8677d394ed76a008f44b45a1be1fb74b6cd3f
racheev <racheev.sa.dev@gmail.com>
2022-03-10 16:34:59 -0500

    5. Очередь на двух стеках [#160 #273148] В методе poll проверка на пустоту очереди и исключение

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,10 +1,19 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
+    public int size() {
+        return in.size() + out.size();
+    }
+
     public T poll() {
+        if (size() == 0) {
+            throw new NoSuchElementException();
+        }
         if (out.size() == 0) {
             while (in.size() != 0) {
                 out.push(in.pop());
